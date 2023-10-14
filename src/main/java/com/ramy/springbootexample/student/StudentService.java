0db771e35eacdd7b838541c4b4cda31fcb978fbb
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2023-10-14 22:24:29 +0100

    implementing the put

--- src/main/java/com/ramy/springbootexample/student/StudentService.java
+++ src/main/java/com/ramy/springbootexample/student/StudentService.java
@@ -2,11 +2,7 @@ package com.ramy.springbootexample.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
